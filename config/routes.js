24f5b804ad6608bccc0d4641b2bcf75a6082ef88
dislersd <dislersd@gmail.com>
2019-04-05 08:49:40 -0700

    register complete - hashes password

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const { authenticate } = require('../auth/authenticate');
 const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,12 +10,15 @@ module.exports = server => {
 };
 
 async function register (req, res) {
-  // try {
-  //   let { username, password } = req.body;
-  //   const user = await 
-  // } catch (error) {
-    
-  // }
+  try {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 2);
+    user.password = hash;
+    const saved = await Users.add(user);
+    res.status(201).json(saved);
+  } catch (error) {
+    res.status(500).json({ message: 'error registering' })
+  }
 }
 
 function login(req, res) {
